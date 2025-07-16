23cebad75fd69df2972d71252c8a7d2bd5f06ff2
Sara Thompson <s34thomp@gmail.com>
2025-07-16 15:10:13 -0400

    Updated HomePage type

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -69,7 +69,7 @@ export function HomePage() {
                     <div className="flex flex-col w-full h-fit">
                         <span className="text-neutral-500 tracking-widest font-semibold">HELLO, MY NAME IS</span>
                         <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl bg-linear-to-r from-neutral-800 to-sky-600 bg-clip-text text-transparent">Sara Thompson</h1>
-                        <p className="pt-6 text-neutral-500 text-xl sm:text-2xl font-light">I’m a frontend designer and UX designer blending thoughtful design with code to create intuitive, user-centered digital experiences.</p>
+                        <p className="pt-6 text-neutral-500 text-xl sm:text-2xl font-light">I’m a product owner and UX designer blending thoughtful design with code to create intuitive, user-centered digital experiences.</p>
                     </div>
                     <div className="flex flex-row gap-4">
                         <Button variant="default" className="bg-linear-to-r from-sky-600 to-sky-500">
